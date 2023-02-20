89613426d43725e65160367b697cb4575a990a99
Jowen Abucejo <jabucejo@yondu.com>
2023-02-20 13:04:36 +0800

    Add test for profile image

--- src/tests/unit/MainNav.test.js
+++ src/tests/unit/MainNav.test.js
@@ -1,5 +1,6 @@
 import MainNav from "../../components/MainNav.vue";
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 
 describe("MainNav", () => {
   it("displays menu items for navigation", () => {
@@ -14,5 +15,27 @@ describe("MainNav", () => {
       "Jobs",
       "Students",
     ]);
+  });
+});
+
+describe("when user logs in", () => {
+  it("display user profile picture", async () => {
+    render(MainNav);
+    screen.debug();
+
+    let profileImage = screen.queryByRole("img", {
+      name: /profile image/i,
+    });
+    expect(profileImage).not.toBeInTheDocument();
+
+    const logInButton = screen.getByRole("button", {
+      name: /sign in/i,
+    });
+    await userEvent.click(logInButton);
+
+    profileImage = screen.queryByRole("img", {
+      name: /profile image/i,
+    });
+    expect(profileImage).toBeInTheDocument();
   });
 });
