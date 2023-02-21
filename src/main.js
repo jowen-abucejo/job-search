86847f60b5a2f0984b0a09dda2bbe0531eb9d839
Jowen Abucejo <jabucejo@yondu.com>
2023-02-21 08:24:02 +0800

    Add and setup font awesome icon globally to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./index.css";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
