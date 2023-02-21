c63cb8035628686ca55f11b942017da5acbd9936
Jowen Abucejo <jabucejo@yondu.com>
2023-02-21 08:24:38 +0800

    Add subnav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,6 +1,7 @@
 <script>
 import ActionButton from "./ActionButton.vue";
 import ProfileImage from "./ProfileImage.vue";
+import SubNav from "./SubNav.vue";
 
 export default {
   name: "MainNav",
@@ -32,32 +33,28 @@ export default {
         "slide-down": this.showDropdownNav,
       };
     },
+
+    headerClass() {
+      return {
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
+      };
+    },
   },
-  components: { ActionButton, ProfileImage },
+  components: { ActionButton, ProfileImage, SubNav },
 };
 </script>
 
 <template>
-  <header class="border-y border-white">
-    <nav class="border-gray-200 bg-brand-purple-1">
+  <header :class="['border-y', 'border-white', headerClass]">
+    <nav class="fixed h-16 w-full border-gray-200">
       <div
-        class="mx-auto flex flex-wrap items-center justify-between lg:justify-start"
+        class="flex flex-wrap items-center justify-start bg-brand-purple-1 lg:justify-start"
       >
-        <a href="/" class="mx-8 flex items-center py-2">
-          <span class="self-center whitespace-nowrap dark:text-white"
-            >Careers at</span
-          >&nbsp;
-          <img
-            src="https://www.yondu.com/wp-content/uploads/2022/01/Yondu-without-Tagline_White.svg"
-            width="80"
-            class="mx-1 mt-1 h-6 sm:h-9"
-            alt="company logo"
-          />
-        </a>
         <button
           @click="toggleDropdownNav"
           type="button"
-          class="mx-8 inline-flex items-center rounded-lg p-2 text-sm text-gray-100 hover:text-white lg:hidden"
+          class="ml-8 inline-flex items-center rounded-lg p-5 pl-0 text-sm text-gray-100 hover:text-white lg:hidden"
         >
           <span class="sr-only">Open main menu</span>
           <svg
@@ -73,37 +70,45 @@ export default {
             ></path>
           </svg>
         </button>
-        <div class="right-0 hidden lg:fixed lg:block">
+        <a href="/" class="flex items-center lg:mx-8">
+          <span class="self-center whitespace-nowrap dark:text-white"
+            >Careers at</span
+          >&nbsp;
+          <img
+            src="https://www.yondu.com/wp-content/uploads/2022/01/Yondu-without-Tagline_White.svg"
+            width="80"
+            class="mx-1 mt-1 h-6 sm:h-9"
+            alt="company logo"
+          />
+        </a>
+        <div class="fixed right-0 top-0 mx-8 block pt-4">
           <ActionButton
             v-if="!isLoggedIn"
             text="Sign in"
             color="secondary"
-            class="mx-8 rounded py-1 px-2 text-sm font-semibold"
+            class="rounded py-1 px-2 text-sm font-semibold"
             @click="logIn"
           />
-          <ProfileImage v-else class="mx-8 h-8 w-8 rounded-3xl bg-white" />
+          <ProfileImage v-else class="h-8 w-8 rounded-3xl bg-white" />
         </div>
         <div
-          class="h-full w-full bg-black bg-opacity-50 text-center lg:h-14 lg:max-h-max lg:w-auto lg:bg-transparent"
+          class="h-full w-full overflow-hidden bg-black bg-opacity-90 text-center lg:h-16 lg:w-auto lg:bg-transparent"
           :class="navListClass"
         >
           <ul
-            class="flex h-full flex-col py-2 lg:mt-0 lg:flex-row lg:items-stretch lg:space-x-8 lg:border-0 lg:py-0 lg:pl-3 lg:text-sm lg:font-medium"
+            class="flex h-full flex-col py-3 lg:mt-0 lg:flex-row lg:items-stretch lg:space-x-8 lg:border-0 lg:py-0 lg:pl-3 lg:text-sm lg:font-medium"
           >
-            <li
-              v-for="navItem in navItems"
-              :key="navItem.label"
-              class="mb-1 flex items-center justify-center"
-            >
+            <li v-for="navItem in navItems" :key="navItem.label">
               <a
                 :href="navItem.link"
-                class="block h-full w-full border-b-white px-2 pt-5 text-white hover:border-b dark:text-white"
+                class="block h-full w-full border-b-white py-3 text-white hover:border-b dark:text-white lg:py-6"
                 >{{ navItem.label }}</a
               >
             </li>
           </ul>
         </div>
       </div>
+      <SubNav v-if="isLoggedIn" />
     </nav>
   </header>
 </template>
---
+++ src/components/SubNav.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "SubNav",
+};
+</script>
+
+<template>
+  <div class="h-16 w-full border-b border-solid bg-white">
+    <div class="flex h-full w-full items-center px-8">
+      <div>
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span><span class="text-green-700">1643</span> Jobs matched</span>
+      </div>
+    </div>
+  </div>
+</template>
