a92e94b27221b6ea4005566eb19e587745474e85
Jowen Abucejo <jabucejo@yondu.com>
2023-02-25 13:21:12 +0800

    Add page for individual job

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import HomePage from "@/components/pages/HomePage.vue";
 import JobResultsPage from "@/components/pages/JobResultsPage.vue";
+import JobDetailsPage from "@/components/pages/JobDetailsPage.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   { path: "/", name: "Home", component: HomePage },
   { path: "/jobs/results", name: "JobResults", component: JobResultsPage },
+  { path: "/jobs/results/:id", name: "JobDetails", component: JobDetailsPage },
 ];
 
 const router = createRouter({ history: createWebHashHistory(), routes });
