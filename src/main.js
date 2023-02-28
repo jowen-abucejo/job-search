2741344b5408dd65cb731a53b99dec7bd8abbb95
Jowen Abucejo <jabucejo@yondu.com>
2023-02-28 11:34:29 +0800

    Add pinia and pinia testing package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 
@@ -9,7 +10,10 @@ import router from "./router";
 
 library.add(faSearch);
 
+const pinia = createPinia();
+
 createApp(App)
+  .use(pinia)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
