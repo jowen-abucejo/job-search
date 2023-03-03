dc0bee1721bf340a0dc15cef88f5316ca082d761
Jowen Abucejo <jabucejo@yondu.com>
2023-03-03 16:17:28 +0800

    Add state and action in user store for unique organizations

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,13 +1,19 @@
 import { defineStore } from "pinia";
 
+export const ADD_SELECTED_ORGANIZATIONS = "ADD_SELECTED_ORGANIZATIONS";
+
 export const useUserStore = defineStore("user", {
   state: () => ({
     isLoggedIn: false,
+    selectedOrganizations: [],
   }),
 
   actions: {
     login() {
       this.isLoggedIn = true;
+    },
+    [ADD_SELECTED_ORGANIZATIONS](organizations) {
+      this.selectedOrganizations = organizations;
     },
   },
 });
--- src/tests/unit/stores/user.test.js
+++ src/tests/unit/stores/user.test.js
@@ -10,6 +10,11 @@ describe("state", () => {
     const store = useUserStore();
     expect(store.isLoggedIn).toBe(false);
   });
+
+  it("stores user selected job organizations", () => {
+    const store = useUserStore();
+    expect(store.selectedOrganizations).toEqual([]);
+  });
 });
 
 describe("actions", () => {
@@ -18,6 +23,14 @@ describe("actions", () => {
       const store = useUserStore();
       store.login();
       expect(store.isLoggedIn).toBe(true);
+    });
+  });
+
+  describe("ADD_SELECTED_ORGANIZATIONS", () => {
+    it("update user selected organizations", () => {
+      const store = useUserStore();
+      store.ADD_SELECTED_ORGANIZATIONS(["Org1", "Org2"]);
+      expect(store.selectedOrganizations).toEqual(["Org1", "Org2"]);
     });
   });
 });
