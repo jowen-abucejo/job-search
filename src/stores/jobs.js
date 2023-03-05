81ae61eabaf2612655b7fef9d6748a65e1a96002
Jowen Abucejo <jabucejo@yondu.com>
2023-03-05 10:57:07 +0800

    Add job types state and filter in store

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -5,6 +5,7 @@ import { useUserStore } from "./user";
 
 export const FETCH_JOBS = "FETCH_JOBS";
 export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
+export const UNIQUE_JOB_TYPES = "UNIQUE_JOB_TYPES";
 
 export const FILTERED_JOBS = "FILTERED_JOBS";
 
@@ -26,15 +27,28 @@ export const useJobsStore = defineStore("jobs", {
       state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
       return uniqueOrganizations;
     },
+
+    [UNIQUE_JOB_TYPES](state) {
+      const uniqueJobTypes = new Set();
+      state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
+      return uniqueJobTypes;
+    },
+
     [FILTERED_JOBS](state) {
       const selectedOrg = useUserStore().selectedOrganizations;
+      const selectedJobTypes = useUserStore().selectedJobTypes;
 
-      if (selectedOrg.length > 0)
-        return state.jobs.filter((job) =>
-          selectedOrg.includes(job.organization)
-        );
+      return state.jobs.filter((job) => {
+        if (selectedOrg.length > 0 && !selectedOrg.includes(job.organization))
+          return false;
+        if (
+          selectedJobTypes.length > 0 &&
+          !selectedJobTypes.includes(job.jobType)
+        )
+          return false;
 
-      return state.jobs;
+        return true;
+      });
     },
   },
 });
