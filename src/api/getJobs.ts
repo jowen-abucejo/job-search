4a16a5dd1b11157a9186bcb444a95d6a61cb7ef4
Jowen Abucejo <jabucejo@yondu.com>
2023-03-07 16:43:46 +0800

    Add job interface and convert  get jobs api call and unit tests to ts files

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import type { Job } from "./types";
 
 const getJobs = async () => {
   const baseUrl = "http://localhost:3000";
-  const response = await axios.get(`${baseUrl}/jobs`);
+  const response = await axios.get<Job[]>(`${baseUrl}/jobs`);
 
   return response.data;
 };
--- src/tests/unit/api/getJobs.test.ts
+++ src/tests/unit/api/getJobs.test.ts
@@ -1,19 +1,22 @@
 import axios from "axios";
 
 import getJobs from "@/api/getJobs";
+import type { Mock } from "vitest";
 
 vi.mock("axios");
 
+const axiosGetMock = axios.get as Mock;
+
 describe("getJobs", () => {
   beforeEach(() => {
-    axios.get.mockResolvedValue({
+    axiosGetMock.mockResolvedValue({
       data: [{ id: 1, title: "Vue Developer" }],
     });
   });
 
   it("fetches jobs the user can apply to", async () => {
     await getJobs();
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+    expect(axiosGetMock).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("extracts jobs from response", async () => {
